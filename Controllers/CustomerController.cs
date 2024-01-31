d1112bd550cfb0a570244538d9e8c2ab6da4cd0c
tonyfung86 <tonyfung86.gmail.com>
2024-01-31 00:03:02 -0500

    Upd: Customized web API

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using CustomerAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers() 
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1,
+                    Name = "Tony",
+                    Email = "tony@gmail.com",
+                    Phone = "12345678",
+                    Address = "123 home Ave E",
+                    City = "Torondo"
+                }
+            };
+            return Ok(customers);
+        }
+    }
+}
---
+++ Entities/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerAPI.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+
+    }
+}

fa37cd6478c7b0df130708915a606f0ae2953a67
tonyfung86 <tonyfung86.gmail.com>
2024-02-05 02:07:45 -0500

    Upd: Put and Delete Data

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 ﻿using CustomerAPI.Data;
 using CustomerAPI.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -60,5 +61,51 @@ namespace CustomerAPI.Controllers
             return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
 
         }
+
+        [HttpPut]
+        public async Task<ActionResult<Customer>> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) 
+            {
+                if (!CustomerExists(id)) { return NotFound(); }
+                else { throw; }
+            }
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return (_context.Customers?.Any(customer => customer.Id == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            if(_context.Customers == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null) 
+            {
+                return NotFound();
+            }
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
+
 }
